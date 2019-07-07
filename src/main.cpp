505a9c673b5a7abb32da8bc768c44bbbd1f00a62
Pawel Raszewski <raszewski@gmail.com>
2019-07-07 02:49:07 -0400

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -9,30 +9,32 @@ const int colorR = 255;
 const int colorG = 255;
 const int colorB = 0;
 
-const int buttonPin = 2; // the number of the pushbutton pin
-const int ledPin = 13;   // the number of the LED pin
+const int weldButtonPin = 3; // the number of the pushbutton pin
+const int menuButtonPin = 2; // the number of the pushbutton pin
 
 // Variables will change:
-int ledState = HIGH;       // the current state of the output pin
-int buttonState;           // the current reading from the input pin
-int lastButtonState = LOW; // the previous reading from the input pin
+int ledState = HIGH;        // the current state of the output pin
+int buttonState;            // the current reading from the input pin
+int lastButtonState = HIGH; // the previous reading from the input pin
 
 // the following variables are unsigned long's because the time, measured in miliseconds,
 // will quickly become a bigger number than can be stored in an int.
 unsigned long lastDebounceTime = 0; // the last time the output pin was toggled
 unsigned long debounceDelay = 50;   // the debounce time; increase if the output flickers
 
-int profileIndex = -1;
+int profileIndex = 0;
 
 int profiles[][3] = {{50, 500, 500},
                      {100, 500, 500},
                      {150, 500, 500}};
 
+void updateProfileScreen();
+
 void setup()
 {
 
-    pinMode(buttonPin, INPUT_PULLUP);
-    pinMode(ledPin, OUTPUT);
+    pinMode(menuButtonPin, INPUT_PULLUP);
+    pinMode(weldButtonPin, INPUT_PULLUP);
     // set up the LCD's number of columns and rows:
     lcd.begin(16, 2);
 
@@ -43,15 +45,13 @@ void setup()
     lcd.setCursor(0, 1);
     lcd.print("made by Pawel");
     delay(300);
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Profile #" + String(profileIndex));
+    updateProfileScreen();
 }
 
 void switchProfile()
 {
     // read the state of the switch into a local variable:
-    int reading = digitalRead(buttonPin);
+    int reading = digitalRead(menuButtonPin);
 
     // check to see if you just pressed the button
     // (i.e. the input went from LOW to HIGH), and you've waited
@@ -64,7 +64,8 @@ void switchProfile()
         lastDebounceTime = millis();
     }
 
-    if ((millis() - lastDebounceTime) + debounceDelay) //put  greater than sign instead of plus (+). i cant do in youtube description{
+    if ((millis() - lastDebounceTime) < debounceDelay)
+    {
         // whatever the reading is at, it's been there for longer
         // than the debounce delay, so take it as the actual current state:
 
@@ -80,20 +81,26 @@ void switchProfile()
                 profileIndex++;
                 profileIndex = profileIndex % (sizeof(profiles) / sizeof(profiles[0]));
 
-                lcd.clear();
-                lcd.setCursor(0, 0);
-                lcd.print("Profile #" + String(profileIndex));
-                lcd.setCursor(0, 1);
-                lcd.print("PW" + String(profiles[profileIndex][0]) + " P" + String(profiles[profileIndex][1]) + " W" + String(profiles[profileIndex][2]));
+                updateProfileScreen();
             }
         }
 
-    // set the LED:
-    // digitalWrite(ledPin, ledState);
+        // set the LED:
+        // digitalWrite(ledPin, ledState);
 
-    // save the reading. Next time through the loop,
-    // it'll be the lastButtonState:
-    lastButtonState = reading;
+        // save the reading. Next time through the loop,
+        // it'll be the lastButtonState:
+        lastButtonState = reading;
+    }
+}
+
+void updateProfileScreen()
+{
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Profile #" + String(profileIndex));
+    lcd.setCursor(0, 1);
+    lcd.print("PW" + String(profiles[profileIndex][0]) + " P" + String(profiles[profileIndex][1]) + " W" + String(profiles[profileIndex][2]));
 }
 
 void loop()
